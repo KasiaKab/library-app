d026c6eeca91872c9f40b152ee1264c0c823aa47
KasiaKab <kkate2402@gmail.com>
2025-07-12 21:53:56 +0200

    Add AuthorController with endpoint GET /authors

---
+++ src/main/java/com/kasiakab/library/controller/AuthorController.java
@@ -0,0 +1,27 @@
+package com.kasiakab.library.controller;
+
+import com.kasiakab.library.dto.AuthorDTO;
+import com.kasiakab.library.service.AuthorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<AuthorDTO> getAllAuthors() {
+        return authorService.getAllAuthors();
+
+    }
+
+}
