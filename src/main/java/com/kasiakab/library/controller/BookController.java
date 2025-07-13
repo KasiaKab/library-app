8bc805ef20825ffa9563a85c035efb140d4342c8
KasiaKab <kkate2402@gmail.com>
2025-07-13 12:46:56 +0200

    Add POST /books endpoint to create a new book with validation

--- src/main/java/com/kasiakab/library/controller/BookController.java
+++ src/main/java/com/kasiakab/library/controller/BookController.java
@@ -1,7 +1,12 @@
 package com.kasiakab.library.controller;
 
 import com.kasiakab.library.dto.BookDTO;
+import com.kasiakab.library.dto.BookRequestDTO;
+import com.kasiakab.library.model.Book;
 import com.kasiakab.library.service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +25,12 @@ public class BookController {
     @GetMapping
     public List<BookDTO> getAllBooks() {
         return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@Valid @RequestBody BookRequestDTO dto) {
+        Book createdBook = bookService.addBook(dto);
+        return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
     }
 
 }
