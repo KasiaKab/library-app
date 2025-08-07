47f7c5f08e54c11728c29cf623858fe0deb8134c
KasiaKab <kkate2402@gmail.com>
2025-08-07 14:40:53 +0200

    Add BorrowController methods GET and POST with DTO mapping

---
+++ src/main/java/com/kasiakab/library/controller/BorrowController.java
@@ -0,0 +1,29 @@
+package com.kasiakab.library.controller;
+
+import com.kasiakab.library.dto.BorrowDTO;
+import com.kasiakab.library.service.BorrowService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/borrow")
+public class BorrowController {
+
+    private final BorrowService borrowService;
+
+    public BorrowController(BorrowService borrowService) {
+        this.borrowService = borrowService;
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<BorrowDTO>> getBorrowsByUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(borrowService.getBorrowsByUserId(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<BorrowDTO> createBorrow(@RequestBody BorrowDTO borrowDTO) {
+        return ResponseEntity.ok(borrowService.createBorrow(borrowDTO));
+    }
+}
