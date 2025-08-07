6ad0f360bcfa8e66845beb4aca8352b51f3c1961
KasiaKab <kkate2402@gmail.com>
2025-08-07 14:06:12 +0200

    Add UserRepository for data access

---
+++ src/main/java/com/kasiakab/library/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kasiakab.library.repository;
+
+import com.kasiakab.library.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
