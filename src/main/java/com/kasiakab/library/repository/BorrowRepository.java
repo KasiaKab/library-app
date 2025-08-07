547f7c5e7c9b0d54bdda628f8ce9e0ca773106c5
KasiaKab <kkate2402@gmail.com>
2025-08-07 14:01:34 +0200

    Add BorrowRepository for data access

---
+++ src/main/java/com/kasiakab/library/repository/BorrowRepository.java
@@ -0,0 +1,11 @@
+package com.kasiakab.library.repository;
+
+import com.kasiakab.library.model.Borrow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BorrowRepository extends JpaRepository<Borrow, Integer> {
+    List<Borrow> findByUserId(Long userId);
+
+}
